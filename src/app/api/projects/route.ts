eb7ee151eada518a946b30ebc8e703937b16543d
Ronald McIntyre <ronxcamacho@gmail.com>
2026-06-15 22:47:51 -0400

    hid put for now

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -35,52 +35,52 @@ export async function GET() {
   }
 }
 
-export async function PUT(request: Request) {
-  try {
-    const project = await request.json();
+// export async function PUT(request: Request) {
+//   try {
+//     const project = await request.json();
 
-    // 1. Load existing blob JSON
-    if (!process.env.NEXT_PUBLIC_JSON_URL) {
-    return NextResponse.json(
-      { error: 'Missing environment variables' }, 
-      { status: 500 }
-    );
-  }
+//     // 1. Load existing blob JSON
+//     if (!process.env.NEXT_PUBLIC_JSON_URL) {
+//     return NextResponse.json(
+//       { error: 'Missing environment variables' }, 
+//       { status: 500 }
+//     );
+//   }
   
 
 
-    const agent = new https.Agent({
-      rejectUnauthorized: false,
-    });
-    const res = await axios.get(process.env.NEXT_PUBLIC_JSON_URL, {
-      httpsAgent: agent,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+//     const agent = new https.Agent({
+//       rejectUnauthorized: false,
+//     });
+//     const res = await axios.get(process.env.NEXT_PUBLIC_JSON_URL, {
+//       httpsAgent: agent,
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
 
-    const data = await res.data.json();
+//     const data = await res.data.json();
 
-    // 2. Update in memory
-    data.projects = data.projects.map((p: any) =>
-      p.id === project.id ? project : p
-    );
+//     // 2. Update in memory
+//     data.projects = data.projects.map((p: any) =>
+//       p.id === project.id ? project : p
+//     );
 
-    // 3. Overwrite blob
-    await put("projects.json", JSON.stringify(data, null, 2), {
-      access: "public", // must match how you want to store it
-      contentType: "application/json",
-      token: process.env.BLOB_READ_WRITE_TOKEN
-    });
+//     // 3. Overwrite blob
+//     await put("projects.json", JSON.stringify(data, null, 2), {
+//       access: "public", // must match how you want to store it
+//       contentType: "application/json",
+//       token: process.env.BLOB_READ_WRITE_TOKEN
+//     });
 
-    return NextResponse.json(project);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to update project: " + error },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(project);
+//   } catch (error) {
+//     return NextResponse.json(
+//       { error: "Failed to update project: " + error },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 // export async function POST(request: Request) {
 //   try {
